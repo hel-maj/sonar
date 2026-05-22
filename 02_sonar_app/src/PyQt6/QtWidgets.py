2b6b7f6b9f2dbf4d7baace2577be547eb419954e
Николай Федоров <helight59@gmail.com>
2026-05-22 18:03:06 +0300

    Map PyQt6 QtWidgets imports to PySide6

---
+++ 02_sonar_app/src/PyQt6/QtWidgets.py
@@ -0,0 +1,1 @@
+from PySide6.QtWidgets import QApplication, QAbstractItemView, QCheckBox, QComboBox, QFileDialog, QFormLayout, QGridLayout, QGroupBox, QHeaderView, QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMessageBox, QPushButton, QScrollArea, QStyledItemDelegate, QTableWidget, QTableWidgetItem, QTabWidget, QVBoxLayout, QWidget
