f775ed82bfa2284c30e11b17a0c9fb54cf08411f
Николай Федоров <helight59@gmail.com>
2026-05-24 05:56:12 +0300

    Update module text

--- 02_sonar_app/src/sonar/telegram/__init__.py
+++ 02_sonar_app/src/sonar/telegram/__init__.py
@@ -1,1 +1,14 @@
 """Telegram integration."""
+
+from sonar.telegram.notifier_adjustments import apply_to_module
+
+
+def install() -> None:
+    import sys
+
+    module = sys.modules.get("sonar.telegram.notifier")
+    if module is not None:
+        apply_to_module(module)
+
+
+install()
